8e0b65d240505a9a8d6a467942f47cce97c67bda
Efsane <efs.gun@gmail.com>
2022-05-29 14:37:40 -0700

    slider and bug fix

---
+++ js/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+
+    $(".slider1").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        lazyLoad: true,
+        autoHeight:true
+    });
+
+    $(".slider2").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        lazyLoad: true,
+        autoHeight:true
+    });
+
+    $(".slider3").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        lazyLoad: true,
+        autoHeight:true
+    });
+});
